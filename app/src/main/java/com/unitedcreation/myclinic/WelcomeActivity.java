c615b2c8afab38eb715a45ac6d4945bf4707bf10
Vishal Rao <vishal.rao103@gmail.com>
2019-02-22 11:02:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/unitedcreation/myclinic/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.unitedcreation.myclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
