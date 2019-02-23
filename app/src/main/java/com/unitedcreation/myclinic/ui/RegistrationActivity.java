7282b255747ef62fbfb338653f547cb5458f0cfe
Vishal Rao <vishal.rao103@gmail.com>
2019-02-23 23:39:47 +0530

    Added maps

--- app/src/main/java/com/unitedcreation/myclinic/ui/RegistrationActivity.java
+++ app/src/main/java/com/unitedcreation/myclinic/ui/RegistrationActivity.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 
 import com.unitedcreation.myclinic.R;
 
+import static com.unitedcreation.myclinic.utils.StringUtils.PROFILE_EXTRA;
+import static com.unitedcreation.myclinic.utils.ViewUtils.switchTheme;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration_patient);
+        switchTheme(this, getIntent().getIntExtra(PROFILE_EXTRA, 0));
+        setContentView(R.layout.activity_registration);
+
+
+
     }
 }
