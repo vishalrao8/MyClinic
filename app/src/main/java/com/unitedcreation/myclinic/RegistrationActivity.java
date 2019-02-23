3123f5b7f3a3ab98278ba03cc83d8d70a81d8481
Abhishek Sharma <itsajusharma@gmail.com>
2019-02-23 15:48:13 +0530

    Commit 02

--- app/src/main/java/com/unitedcreation/myclinic/RegistrationActivity.java
+++ app/src/main/java/com/unitedcreation/myclinic/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.unitedcreation.myclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import durdinapps.rxfirebase2.RxFirebaseAuth;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
     }
 }
